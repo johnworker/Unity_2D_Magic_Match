20466e216150cbbccf7b709dcae33f056434234d
hunte <besscomdark@gmail.com>
2025-01-26 18:28:24 +0800

    調整

--- Assets/GemHunterMatch/Scripts/Authoring/GemSpawner.cs
+++ Assets/GemHunterMatch/Scripts/Authoring/GemSpawner.cs
@@ -1,12 +1,12 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Tilemaps;
 
 namespace Match3
 {
     /// <summary>
-    /// This is an edit time tile, used to define the position of cell that will spawn cell under them. At edit time and outside
-    /// play mode, the PReview Sprite will be displayed on the tilemap but at runtime, the tile will be empty as its only a
-    /// game logic placement.
+    /// 這是一個編輯時間圖塊，用於定義將在其下方產生單元格的單元格的位置。在編輯時和編輯之外
+    /// 在播放模式中，預覽精靈將顯示在地圖上，但在運行時，地圖將為空，因為它只是一個
+    /// 遊戲邏輯佈局。
     /// </summary>
     [CreateAssetMenu(fileName = "GemSpawnerPlacerTile", menuName = "2D Match/Tile/Gem Spawner Placer")]
     public class GemSpawner : TileBase
@@ -24,9 +24,9 @@ namespace Match3
             if (!Application.isPlaying)
                 return false;
 #endif
-        
-            //This tile is only used in editor to help design the level. At runtime, we notify the board that this tile is
-            //a place for a gem. The Board will take care of creating a gem there.
+
+            //此圖塊僅用於編輯器中幫助設計關卡。在運行時，我們通知板，此圖塊是
+            //寶石的產地。董事會將負責在那裡打造一顆璀璨的寶石。
             Board.RegisterSpawner(position);
 
             return base.StartUp(position, tilemap, go);
